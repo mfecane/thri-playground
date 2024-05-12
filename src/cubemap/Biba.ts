5f8ccb49fb129cf2dd8eb398f40d046b09a30441
Aleksei Aliapkin <aaliapkin@gmail.com>
2024-05-12 14:50:54 +0200

    failed cubemap sample to blur

--- src/cubemap/Biba.ts
+++ src/cubemap/Biba.ts
@@ -1,16 +1,27 @@
 import {
+	AxesHelper,
 	EquirectangularReflectionMapping,
+	NoBlending,
 	PMREMGenerator,
 	PerspectiveCamera,
 	Scene,
+	ShaderMaterial,
 	Texture,
+	Vector2,
 	WebGLRenderer,
 } from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { EXRLoader } from 'three/examples/jsm/loaders/EXRLoader.js'
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js'
+import { FullScreenQuad } from 'three/examples/jsm/postprocessing/Pass.js'
+
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js'
 import { BokehPass } from 'three/examples/jsm/postprocessing/BokehPass.js'
+
+import depthVert from './shaders/quad_vert.glsl'
+import depthFrag from './shaders/cube_sampler_blurred.glsl'
+
+let mMaterial: ShaderMaterial
 
 export class Biba {
 	public renderer = new WebGLRenderer()
@@ -23,7 +34,29 @@ export class Biba {
 
 	private composer = new EffectComposer(this.renderer)
 
-	public constructor(private width: number, private height: number) {}
+	private fsQuad = new FullScreenQuad()
+
+	public constructor(private width: number, private height: number) {
+		mMaterial = new ShaderMaterial({
+			defines: {
+				ENVMAP_TYPE_CUBE_UV: 1,
+				CUBEUV_TEXEL_WIDTH: 1 / this.width,
+				CUBEUV_TEXEL_HEIGHT: 1 / this.height,
+				CUBEUV_MAX_MIP: `0.0`,
+			},
+			uniforms: {
+				cameraWorldMatrix: { value: null },
+				cameraProjectionMatrixInverse: { value: null },
+				resolution: { value: null },
+				envMap: { value: null },
+			},
+			vertexShader: depthVert,
+			fragmentShader: depthFrag,
+			blending: NoBlending,
+			depthTest: false,
+			depthWrite: false,
+		})
+	}
 
 	public async init() {
 		this.renderer.setSize(this.width, this.height)
@@ -35,17 +68,29 @@ export class Biba {
 				return resolve(rt.texture)
 			})
 		)
-		this.scene.background = texture
-		this.scene.backgroundBlurriness = 0.3
-		this.scene.backgroundIntensity = 0.01
+		// this.scene.background = texture
+		this.scene.backgroundBlurriness = 0.0
+		this.scene.backgroundIntensity = 0.02
 		this.camera.position.set(0.0, 0.0, -5.0)
 
-		this.composer.addPass(new RenderPass(this.scene, this.camera))
+		// this.composer.addPass(new RenderPass(this.scene, this.camera))
 		// this.composer.addPass(new BokehPass(this.scene, this.camera, { focus: 1.0, aperture: 0.5, maxblur: 0.03 }))
+
+		mMaterial.uniforms.cameraWorldMatrix.value = this.camera.matrixWorld
+		mMaterial.uniforms.cameraProjectionMatrixInverse.value = this.camera.projectionMatrixInverse.clone()
+		mMaterial.uniforms.resolution.value = new Vector2(this.width, this.height)
+		mMaterial.uniforms.envMap.value = texture
+		this.fsQuad.material = mMaterial
 	}
 
 	public render() {
 		this.controls.update()
-		this.composer.render()
+
+		// this.composer.render()
+		this.fsQuad.render(this.renderer)
+		const s = new Scene()
+		this.renderer.autoClear = false
+		s.add(new AxesHelper())
+		this.renderer.render(s, this.camera)
 	}
 }
